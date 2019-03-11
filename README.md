01659e44810480779f748635fac7ba28155c9bf0
AustinOsti <austin.nyika2015@gmail.com>
2019-03-11 16:02:45 +0300

    add delete file, etc

--- README.md
+++ README.md
@@ -11,11 +11,8 @@ Scrap Football Results
 5. Select the saved web page and open it in a new page.
 6. Right click on mouse and select View page source
 7. Select all the content on the page source displayed in the new page.
-8. Paste the content in Notepad++ and save the file as sample.html format in the project folder.
-9. In the server.js file, make the following changes:
-9.1. Set the setFullYear as setFullYear(yy, m-1, d), where yy represents current year, m - current month, d current day
-10. Save the changes to the server.js file
-11. At the command prompt, run npm start
+8. Paste the content in Notepad++ and save the file as sample.html in the project folder.
+9. At the command prompt, run npm start
 
 ### Convert JSON to HTML
 1. Navigate to the url http://convertjson.com/json-to-html-table.htm
@@ -27,4 +24,4 @@ Scrap Football Results
 ### Append the results data to the BetAnalysis.xls spreadsheet
 1. Open the Betanalysis spreadsheet.
 2. Place the curser after the last occupied row.
-3. Copy the converted data in the Result Data box and paste it at the cusror position in the Bet analysis spreadhseet.
+3. Copy the converted data in the Result Data box of the JSON to HTML convertor website and paste it at the cusror position in the Bet analysis spreadsheet.
--- server.js
+++ server.js
@@ -2,20 +2,86 @@ const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-// send console.log to tips.txt file ...
+// send extracted data to tips.txt file ...
 const file = fs.createWriteStream('./tips.txt');
 
 const archiveFile = 'sample.html';
 const url = 'https://www.soccer24.com/';
 
+// set the date of the results 
+//	d = new Date().setFullYear(2019, 2, 10);
+d = new Date();
+d.setDate(d.getDate() - 1);
+
 //* option 1 - use fs to retrieve archived results stored an a local file as sample.html
-fs.readFile(archiveFile, 'utf-8', function(err, data) {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	d = new Date().setFullYear(2019, 2, 9);
-	getData(data, d);	
+let extractData = function(archiveFile, d) {
+	return new Promise(function(resolve, reject) {		
+		fs.readFile(archiveFile, 'utf-8', function(err, data) {
+			if (err) {
+				reject(err);
+			}		
+			resolve(data);
+		});	
+	});
+}
+
+//parse the HTML with Cheerio.js
+let getData = function(html, d){
+	return new Promise(function(resolve, reject) {		
+		data = [];	
+		let league = {};
+		const $ = cheerio.load(html);
+		$('table.soccer tr')
+		.each(function(i, elem){
+			const results = $(elem).children("td:nth-child(6)").text();		
+			if (results.length === 0) {
+				league = {
+					a: $(elem).children("td:nth-child(2)").text()
+				}			
+				Object.freeze(league);
+			}	
+			if (results.length > 0) {
+				data.push({
+					date: new Date(d),
+					time: $(elem).children("td:nth-child(2)").text(),
+					status: $(elem).children("td:nth-child(3)").text(),
+					league: league.a.replace("Standings", ""),
+					home: $(elem).children("td:nth-child(4)").text(),
+					away: $(elem).children("td:nth-child(5)").text(),
+					results: $(elem).children("td:nth-child(6)").text(),
+					odds_1:	$(elem).children("td:nth-child(8)").text(),
+					odds_X: $(elem).children("td:nth-child(9)").text(),
+					odds_2: $(elem).children("td:nth-child(10)").text()
+				});			
+			}	
+		});
+		file.write(JSON.stringify(data));
+		file.end();
+		resolve();
+	});
+}
+
+// delete the archived data file to prevent duplicates
+let deleteDataFile = function(fileToDelete) {
+	return new Promise(function(resolve, reject) {	
+		fs.unlink(fileToDelete, function (err) {
+			if (err) throw err;
+			// if no error, file has been deleted successfully
+			console.log('File deleted!');
+		});	
+		resolve();
+	});
+}
+
+var promise = extractData(archiveFile, d);
+promise.then(function(data) {
+	return getData(data, d);
+})			
+.then(function() {
+	return deleteDataFile(archiveFile);
+})
+.catch(function (err) {	
+	console.log(err);
 });
 
 /* option2 - for current results, use axios to retrive results ... under WIP ...
@@ -30,35 +96,4 @@ axios.get(url)
 }); 
 */
 
-//parse the HTML with Cheerio.js
-let getData = function(html, d){
-	data = [];	
-	let league = {};
-	const $ = cheerio.load(html);
-	$('table.soccer tr')
-	.each(function(i, elem){
-		const results = $(elem).children("td:nth-child(6)").text();		
-		if (results.length === 0) {
-			league = {
-				a: $(elem).children("td:nth-child(2)").text()
-			}			
-			Object.freeze(league);
-		}	
-		if (results.length > 0) {
-			data.push({
-				date: new Date(d),
-				time: $(elem).children("td:nth-child(2)").text(),
-				status: $(elem).children("td:nth-child(3)").text(),
-				league: league.a.replace("Standings", ""),
-				home: $(elem).children("td:nth-child(4)").text(),
-				away: $(elem).children("td:nth-child(5)").text(),
-				results: $(elem).children("td:nth-child(6)").text(),
-				odds_1:	$(elem).children("td:nth-child(8)").text(),
-				odds_X: $(elem).children("td:nth-child(9)").text(),
-				odds_2: $(elem).children("td:nth-child(10)").text()
-			});			
-		}	
-	});
-	file.write(JSON.stringify(data));
-	file.end();
-}
+
